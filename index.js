85cd2661aca634d0d50471798915d634b385a9c0
KBis <bismarkkwabenayamoah@gmail.com>
2024-05-23 14:27:37 +0000

    Set up environment

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const app = express();
 const baseDirectory = path.join(__dirname,'/dist');
 
 app.use('/',express.static(baseDirectory))
-// app.get('/',(req,res)=>{
-//     res.setHeader('Content-Type','text/html').send(fs.readFileSync(`${baseDirectory}/trending.html`,'utf8'))
-// })
-const PORT = 30003;
+const PORT = 3003;
 app.listen(PORT,()=>{
     console.log('Serving on port '+PORT);
 })
