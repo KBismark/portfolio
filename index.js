42f214a3a91c7c9cd5ca4d07a2105542e7f310f2
KBismark <bismarkkwabenayamoah@gmail.com>
2024-10-14 18:44:17 +0000

    Update server port test

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 
-// const {execSync} = require('child_process');
-const express = require('express');
-const path = require('path');
-// const fs = require('fs');
-const app = express();
+// // const {execSync} = require('child_process');
+// const express = require('express');
+// const path = require('path');
+// // const fs = require('fs');
+// const app = express();
 
-const baseDirectory = path.join(__dirname);
-// app.use((req, res,next)=>{
-//     console.log(req.url);
-//     if(req.url === '/'){
-//        return res.status(200).send(fs.readFileSync(path.join(__dirname, '/index.html'), 'utf8'))
-//     }
-//     next()
+// const baseDirectory = path.join(__dirname);
+// // app.use((req, res,next)=>{
+// //     console.log(req.url);
+// //     if(req.url === '/'){
+// //        return res.status(200).send(fs.readFileSync(path.join(__dirname, '/index.html'), 'utf8'))
+// //     }
+// //     next()
 
-// })  design-o1tk.onrender.com
+// // })  design-o1tk.onrender.com
 
-app.use('/',express.static(baseDirectory))
+// app.use('/',express.static(baseDirectory))
 
-const PORT = 80;
-app.listen(()=>{
-    console.log('Serving on port '+PORT);
-})
+// const PORT = 80;
+// app.listen(()=>{
+//     console.log('Serving on port '+PORT);
+// })
 
 
-// var http = require('http');
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     var message = 'It works!\n',
-//         version = 'NodeJS ' + process.versions.node + '\n',
-//         response = [message, version].join('\n');
-//     res.end(response);
-// });
-// server.listen();
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen();
 
