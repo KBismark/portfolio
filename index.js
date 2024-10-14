fe62d13d506cebbecb45505520fea74164e5894e
KBismark <bismarkkwabenayamoah@gmail.com>
2024-10-14 19:03:52 +0000

    Update server port

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
-// // const {execSync} = require('child_process');
+// const {execSync} = require('child_process');
 // const express = require('express');
-// const path = require('path');
-// // const fs = require('fs');
+const path = require('path');
+// const fs = require('fs');
 // const app = express();
 
-// const baseDirectory = path.join(__dirname);
-// // app.use((req, res,next)=>{
-// //     console.log(req.url);
-// //     if(req.url === '/'){
-// //        return res.status(200).send(fs.readFileSync(path.join(__dirname, '/index.html'), 'utf8'))
-// //     }
-// //     next()
+const baseDirectory = path.join(__dirname);
+// app.use((req, res,next)=>{
+//     console.log(req.url);
+//     if(req.url === '/'){
+//        return res.status(200).send(fs.readFileSync(path.join(__dirname, '/index.html'), 'utf8'))
+//     }
+//     next()
 
-// // })  design-o1tk.onrender.com
+// })  design-o1tk.onrender.com
 
 // app.use('/',express.static(baseDirectory))
 
@@ -24,12 +24,14 @@
 
 
 var http = require('http');
-var server = http.createServer(function(req, res) {
+var server = http.createServer(
+    function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
+        version = 'NodeJS ' + process.versions.node + '\nRoot: ',
+        response = [message, version, baseDirectory].join('\n');
     res.end(response);
-});
+}
+);
 server.listen();
 
